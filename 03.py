dc36b86f05c97b17cfbdf3035062add514f75c0e
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-12-22 20:11:53 +0800

    practice

---
+++ 03.py
@@ -0,0 +1,21 @@
+var_1 = 3454654465464
+print(type(var_1))
+
+var_2 = 123
+var_3 = 10.1
+print(var_2 + var_3)
+print(type(var_2))
+print(type(var_3))
+
+print(0b10) # Change Binary to Decimal Output is 2
+print(0o10) # Change Octal to Decimal Output is 8
+print(0x10) # Change HexaDecimal to Decimal Output is 16
+
+name = "yash chhatrala,"
+print(name[6])
+
+name_1 = "Yash chhatala"
+name_2 = " Data Science Student"
+print(name_1 + name_2)
+
+print(5 * "Yash\'s \n \"Lactures\"")
