5712539dba52e7925880fa8119b30e12cf3aae8a
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-12-24 00:43:51 +0800

    arithmetic operators

---
+++ 05.py
@@ -0,0 +1,14 @@
+# Arithmetic => + - * / %
+print(4/2) # 2.0
+print(5%2) # 1
+print(4//2) # 2
+print(2**3) # 8
+print(5 + 2 * 3) # 11
+
+# P E M D A S
+# ()
+# ** R->L
+# * / // % L->R
+# + - L->R
+
+print(5 + 2 * 3 - 1 + 10 / 5)
