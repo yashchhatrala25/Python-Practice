dc12ccd135cd156737a51dd47757950dc406159b
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-12-24 01:10:13 +0800

    practice

---
+++ 06.py
@@ -0,0 +1,15 @@
+# Assignment Operators
+# += -= *= /= //= %= **=
+# a = 5
+# a = a + 2
+# print(a)
+
+a, b, c = 5, 6, 7
+print(a, b, c)
+
+
+# Comparison Operators
+# < > <= >= == !=
+a = 5
+print(a < 5)
+print(a == 5)
